f5c72d274b95a38edc36fcdaf47a25e10e7186b0
jlazar17 <jeff.p.lazar@gmail.com>
2022-08-25 15:27:46 -0400

    More descriptive name

--- beansi/noise_box.py
+++ beansi/noise_box.py
@@ -3,9 +3,9 @@ import numpy as np
 def uncorrelated_noise(rate, delta_t, rand=None):
     if rand is None:
         rand = np.random.RandomState()
-    rate  /= 1e9 # Convert to gigahertz
+    rate /= 1e9 # Convert to gigahertz
     n_events = np.random.poisson(rate*delta_t)
-    times    = np.random.uniform(0, delta_t, n_events)
+    times = np.random.uniform(0, delta_t, n_events)
     return times
 
 def correlated_noise(rate, delta_t, mu=-6, sigma=2.7, eta=8., rand=None):
@@ -15,11 +15,11 @@ def correlated_noise(rate, delta_t, mu=-6, sigma=2.7, eta=8., rand=None):
 
     params:
     ______
-    rate    (float) expected rate for radioactive decay [Hz]
+    rate (float) expected rate for radioactive decay [Hz]
     delta_t (float) Total duration of event [ns]
-    mu      (float) Parameter describing the mean delay of photons after radioactive decay
-    sigma   (float) Parameter describing the time spread of photons after radioactive decay
-    eta     (float) Poisson expectation of number of photons per radioactive decay
+    mu (float) Parameter describing the mean delay of photons after radioactive decay
+    sigma (float) Parameter describing the time spread of photons after radioactive decay
+    eta (float) Poisson expectation of number of photons per radioactive decay
 
     returns
     _______
@@ -28,12 +28,14 @@ def correlated_noise(rate, delta_t, mu=-6, sigma=2.7, eta=8., rand=None):
 
     if rand is None:
         rand = np.random.RandomState()
-    mu    += 9 # nano-ify
-    rtimes = uncorrelated_noise(rate, delta_t) # times when radioactive decays happened
-    ptimes = np.array([], dtype=float)
-    for time in rtimes:
+    mu += 9 # nano-ify
+    # Times when radioactive decays happened
+    decay_ts = uncorrelated_noise(rate, delta_t, rand=rand)
+    photon_ts = np.array([], dtype=float)
+    for time in decay_ts:
         n_photons = rand.poisson(eta)
-        zz        = rand.normal(size=n_photons)
-        dtt       = np.power(10., mu+sigma*zz)
-        ptimes    = np.append(ptimes, time+np.cumsum(dtt))
-    return ptimes
+        zz = rand.normal(size=n_photons)
+        dtt = np.power(10., mu + sigma * zz)
+        print(dtt)
+        photon_ts = np.append(photon_ts, time+np.cumsum(dtt))
+    return photon_ts
