06f482ad0545fc8136bf054ce1ba0a7a7f15a40d
Jeffrey Lazar <jlazar@holy7c18306.rc.fas.harvard.edu>
2022-11-17 01:38:07 -0500

    Fix big in SMT handling

--- smt.py
+++ smt.py
@@ -1,95 +1,79 @@
 import numpy as np
 
-def SMT(
-    str_id,
-    sensor_id,
-    t,
-    multiplicity = 8,
-    hlc_dt = 5000,
-    time_window_back = 4e3,
-    time_window_forward = 6e3,
-    return_trange = False
-):
-    str_sorter = np.argsort(str_id)
-    # sort the hits by which string they on since only
-    # OMs on the same string can produce a HLC
-    str_id, sensor_id, t = (
-        str_id[str_sorter], sensor_id[str_sorter], t[str_sorter]
-    )
-    # Split all the stuff by string
-    str_id_split, splitter = consecutive(str_id, return_splitter=True)
-    sensor_id_split = np.split(sensor_id, splitter)
-    t_split = np.split(t, splitter)
-    # Iterate over the hits on each string
-    hlc_times = np.array([])
-    for a, b in zip(
-        sensor_id_split,
-        t_split
-    ):
-        passed = False
-        # Sort since HLCs only happen in one direction
-        t_sorter = np.argsort(b)
-        a, b = a[t_sorter], b[t_sorter]
-        idx = 0
-        for idx in range(len(a)):
-            current_om, current_t = a[idx], b[idx]
-            slc = slice(idx+1, None)
-            # Check if the Oms are neighboring or next-to-neighboring
-            is_neighbor = np.logical_and(
-                a[slc]!=current_om,
-                np.abs(a[slc]-current_om)<=2
-            )
-            is_time_coincident = (b[slc] - current_t) < hlc_dt
-            is_hlc = np.logical_and(is_neighbor, is_time_coincident)
-            hlc_times = np.append(hlc_times, b[slc][is_hlc])
-            if (
-                len(hlc_times) >= multiplicity and \
-                np.max(hlc_times) - np.min(hlc_times) < hlc_dt
-            ):
-                passed = True
-                if return_trange:
-                    return passed, np.min(hlc_times)-time_window_back, np.max(hlc_times) + time_window_forward
-                else:
-                    return passed
-    # TODO do the complicated thing but I don't think it's necessary
-    if return_trange:
-        return passed, np.min(hlc_times)-time_window_back, np.max(hlc_times) + time_window_forward
-    else:
-        return passed
+from collections.abc import Iterable
 
-def consecutive(data, stepsize=0, return_splitter=False):
-    splitter = np.where(np.diff(data) != stepsize)[0]+1
-    if return_splitter:
-        return np.split(data, splitter), splitter
-    else:
-        return np.split(data, splitter)
+class TimesNotOrderedError(Exception):
+
+    def __init__(self):
+        self.message = "Input times are not in ascending order"
+        super().__init__(self.message)
+
+class IncompatibleLengthsError(Exception):
+
+    def __init__(self, id_type, nts, nids):
+        self.message = f"Number of {id_type} incompatible with number of times."
+        self.message += f"Expected {nts} but got {nids}"
+        super().__init__(self.message)
+
+def SMT(
+    string_ids: Iterable[int],
+    sensor_ids: Iterable[int],
+    times: Iterable[float],
+    multiplicity:int = 8,
+    hlc_dt:float = 5000.0,
+) -> bool:
+    """
+    Function to determine if event passed SMT-N
+    """
+    # Make sure lengths are compatible
+    ntimes = len(times)
+    if len(sensor_ids) != ntimes:
+        raise IncompatibleLengthsError("sensor_ids", ntimes, len(sensor_ids))
+    if len(string_ids) != ntimes:
+        raise IncompatibleLengthsError("string_ids", ntimes, len(string_ids))
+
+    # Make sure times are ordered
+    if np.any(np.diff(times) < 0):
+        raise TimesNotOrderedError
+
+    times = np.array(times)
+    sensor_ids = np.array(sensor_ids)
+    string_ids = np.array(string_ids)
+    n_hlc = 0
+    for idx, (time, sensor_id, string_id) in enumerate(zip(times, sensor_ids, string_ids)):
+        slc = slice(idx+1, None, None)
+        is_neighbor = np.logical_and(
+            0 < np.abs(sensor_ids[slc] - sensor_id),
+            np.abs(sensor_ids[slc] - sensor_id) <= 2
+        )
+        is_samestring = string_ids[slc] == string_id
+        can_hlc = np.logical_and(is_neighbor, is_samestring)
+        n_hlc += np.count_nonzero(times[slc][can_hlc] - time)
+        if n_hlc >= multiplicity:
+            break
+    return n_hlc >= multiplicity
 
 def passed_SMT(
     event,
-    multiplicity = 8,
-    hlc_dt = 5000,
-    time_window_forward = 6000,
-    time_window_back = 4000,
-    return_trange = False
+    multiplicity: int = 8,
+    hlc_dt:float = 5000.0,
 ):
     if "string_id" in event.total.fields:
-        str_id = np.array(
+        string_ids = np.array(
             [x for x in event.total.string_id if x != -1]
         )
     else:
-        str_id = np.array(
+        string_ids = np.array(
             [x for x in event.total.sensor_string_id if x != -1]
         )
-    t = np.array([x for x in event.total.t if x != -1])
-    sensor_id = np.array([x for x in event.total.sensor_id if x != -1])
+    times = np.array([x for x in event.total.t if x != -1])
+    sensor_ids = np.array([x for x in event.total.sensor_id if x != -1])
 
+    sorter = np.argsort(times)
     return SMT(
-        str_id,
-        sensor_id,
-        t,
+        string_ids[sorter],
+        sensor_ids[sorter],
+        times[sorter],
         multiplicity = multiplicity,
         hlc_dt = hlc_dt,
-        time_window_back = time_window_back,
-        time_window_forward = time_window_forward,
-        return_trange = return_trange
     )
